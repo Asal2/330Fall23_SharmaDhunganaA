4c6c257b06eca4ad5ab5fa37a1617b0de4075410
Ashal Sharma <ashalsharma68@gmail.com>
2023-09-01 16:11:51 -0500

    lesson2

--- lessons/02/HelloKotlin/src/main/kotlin/Hello.kt
+++ lessons/02/HelloKotlin/src/main/kotlin/Hello.kt
@@ -1,50 +1,40 @@
+//val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+fun main() {
+    val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
 
-import java.util.*    // required import
-import java.util.Random
+    // eager, creates a new list
+    val eager = decorations.filter { it[0] == 'p' }
+    println("eager: $eager")
 
-fun feedTheFish(){
-    val day = randomDay()
-    val food = fishFood(day)
-    println("Today is $day and the fish eat $food")
-    println("Change water: ${shouldChangeWater(day)}")
-}
-fun randomDay(): String{
-    val week = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
-    return week[Random().nextInt(week.size)]
-}
+//     lazy, will wait until asked to evaluate
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println("filtered: $filtered")
 
+    // force evaluation of the lazy list
+    val newList = filtered.toList()
+    println("new list: $newList")
 
-
-fun fishFood(day:String): String{
-    return when (day) {
-        "Monday" -> "flakes"
-        "Wednesday" -> "redworms"
-        "Thursday" -> "granules"
-        "Friday" -> "mosquitoes"
-        "Sunday" -> "plankton"
-        else -> "nothing"
+    val lazyMap = decorations.asSequence().map {
+        println("access: $it")
+        it
     }
-}
+    println("lazy: $lazyMap")
+    println("-----")
+    println("first: ${lazyMap.first()}")
+    println("-----")
+    println("all: ${lazyMap.toList()}")
 
-fun swim(speed: String = "fast"){
-    println("swimming $speed")
-}
-fun isTooHot(temperature: Int) = temperature > 30
-
-fun isDirty(dirty: Int) = dirty > 30
-
-fun isSunday(day: String) = day == "Sunday"
-fun shouldChangeWater (day: String, temperature: Int =22, dirty: Int = 20): Boolean{
-    return when{
-        isTooHot(temperature) -> true
-        isDirty(dirty) -> true
-        isSunday(day) -> true
-        else -> false
+    val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
+        println("access: $it")
+        it
     }
-}
-fun main(args: Array<String>) {
-    feedTheFish()
-    swim()
-    swim("Slow")
-    swim(speed = "trutle-like")
+    println("-----")
+    println("filtered: ${lazyMap2.toList()}")
+
+    val mysports = listOf("basketball", "fishing", "running")
+    val myplayers = listOf("LeBron James", "Ernest Hemingway", "Usain Bolt")
+    val mycities = listOf("Los Angeles", "Chicago", "Jamaica")
+    val mylist = listOf(mysports, myplayers, mycities)     // list of lists
+    println("-----")
+    println("Flat: ${mylist.flatten()}")
 }
