f8b84d1b643729a070536fdad8b3a030f5eae2a0
Ashal Sharma <ashalsharma68@gmail.com>
2023-09-01 15:05:22 -0500

    lesson2

--- lessons/02/HelloKotlin/src/main/kotlin/Hello.kt
+++ lessons/02/HelloKotlin/src/main/kotlin/Hello.kt
@@ -6,11 +6,14 @@ fun feedTheFish(){
     val day = randomDay()
     val food = fishFood(day)
     println("Today is $day and the fish eat $food")
+    println("Change water: ${shouldChangeWater(day)}")
 }
 fun randomDay(): String{
     val week = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
     return week[Random().nextInt(week.size)]
 }
+
+
 
 fun fishFood(day:String): String{
     return when (day) {
@@ -23,7 +26,25 @@ fun fishFood(day:String): String{
     }
 }
 
+fun swim(speed: String = "fast"){
+    println("swimming $speed")
+}
+fun isTooHot(temperature: Int) = temperature > 30
+
+fun isDirty(dirty: Int) = dirty > 30
+
+fun isSunday(day: String) = day == "Sunday"
+fun shouldChangeWater (day: String, temperature: Int =22, dirty: Int = 20): Boolean{
+    return when{
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+        else -> false
+    }
+}
 fun main(args: Array<String>) {
     feedTheFish()
-
+    swim()
+    swim("Slow")
+    swim(speed = "trutle-like")
 }
