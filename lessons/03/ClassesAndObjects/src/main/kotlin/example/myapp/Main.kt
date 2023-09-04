14ba2112229b1e16500baec04f7ac2075c9ca0a3
Ashal Sharma <ashalsharma68@gmail.com>
2023-09-03 22:44:54 -0500

    Subclass and inhertance

--- lessons/03/ClassesAndObjects/src/main/kotlin/example/myapp/Main.kt
+++ lessons/03/ClassesAndObjects/src/main/kotlin/example/myapp/Main.kt
@@ -1,6 +1,8 @@
 package example.myapp
 
 import example.myapp.example.myapp.Aquarium
+import example.myapp.example.myapp.TowerTank
+
 
 fun buildAquarium() {
 //    val aquarium1 = Aquarium()
@@ -15,10 +17,13 @@ fun buildAquarium() {
 //    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
 //    aquarium4.printSize()
 
-    val aquarium6 = Aquarium(numberOfFish = 29)
+    val aquarium6 = Aquarium(length = 25, width = 25, height = 40)
     aquarium6.printSize()
-    aquarium6.volume = 70
-    aquarium6.printSize()
+//    aquarium6.volume = 70
+//    aquarium6.printSize()
+
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
 }
 
 fun main(){
