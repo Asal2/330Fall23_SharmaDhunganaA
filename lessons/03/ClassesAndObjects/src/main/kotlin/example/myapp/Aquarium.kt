f8a7eee9ff86d0d5d4a6313bd572fcf01d90cdc1
Ashal Sharma <ashalsharma68@gmail.com>
2023-09-03 22:06:06 -0500

    Add class constructors

--- lessons/03/ClassesAndObjects/src/main/kotlin/example/myapp/Aquarium.kt
+++ lessons/03/ClassesAndObjects/src/main/kotlin/example/myapp/Aquarium.kt
@@ -5,20 +5,23 @@ class Aquarium(var length: Int = 100,  var width: Int = 20, var height: Int = 40
         println("aquarium initializing")
     }
 
-    init {
-        // 1 liter = 1000 cm^3
-        println("Volume: ${width * length * height / 1000} liters")
-    }
-
     constructor(numberOfFish: Int): this(){
         val tank = numberOfFish * 2000 * 1.1
 
         height = (tank / (length * width)).toInt()
     }
+
+    var volume: Int
+        get() = width * height * length / 1000
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
     fun printSize(){
         println("Width: $width cm " +
                 "Length: $length cm" +
                 "Height: $height cm")
+        println("Volume: $volume liters")
     }
+
 }
 
