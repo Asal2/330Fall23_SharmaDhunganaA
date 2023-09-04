b42b173fb87bbc8bbd6518afee0cd011a8ff7c86
Ashal Sharma <ashalsharma68@gmail.com>
2023-09-04 14:47:50 -0500

    Use interface delegation

--- lessons/03/ClassesAndObjects/src/main/kotlin/example/myapp/AquariumFish.kt
+++ lessons/03/ClassesAndObjects/src/main/kotlin/example/myapp/AquariumFish.kt
@@ -1,24 +1,33 @@
 package example.myapp
 
+interface FishAction  {
+    fun eat()
+}
 abstract class AquariumFish : FishAction {
     abstract val color: String
     override fun eat() = println("yum")
 }
 
-class Shark: AquariumFish(), FishAction {
+class Shark: FishAction, FishColor {
     override val color = "grey"
     override fun eat() {
         println("hunt and eat fish")
     }
 }
 
-class Plecostomus: AquariumFish(), FishAction {
+interface FishColor {
+    val color: String
+}
+class Plecostomus (fishColor: FishColor = GoldColor):
+    FishAction by PrintingFishAction("eat algae"),
+    FishColor by fishColor
+
+object GoldColor : FishColor {
     override val color = "gold"
-    override fun eat() {
-        println("eat algae")
-    }
 }
 
-interface FishAction  {
-    fun eat()
+class PrintingFishAction(val food: String) : FishAction {
+    override fun eat() {
+        println(food)
+    }
 }
