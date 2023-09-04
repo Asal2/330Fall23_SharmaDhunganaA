3d1c98f51ab7a2b4887eaacece74c9126129b0dd
Ashal Sharma <ashalsharma68@gmail.com>
2023-09-03 21:54:20 -0500

    Aquarium class added

--- lessons/03/ClassesAndObjects/src/main/kotlin/example/myapp/Aquarium.kt
+++ lessons/03/ClassesAndObjects/src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,10 +1,20 @@
 package  example.myapp.example.myapp
 
-class Aquarium {
-    var width: Int =20
-    var height: Int = 40
-    var length: Int = 100
+class Aquarium(var length: Int = 100,  var width: Int = 20, var height: Int = 40) {
+    init {
+        println("aquarium initializing")
+    }
 
+    init {
+        // 1 liter = 1000 cm^3
+        println("Volume: ${width * length * height / 1000} liters")
+    }
+
+    constructor(numberOfFish: Int): this(){
+        val tank = numberOfFish * 2000 * 1.1
+
+        height = (tank / (length * width)).toInt()
+    }
     fun printSize(){
         println("Width: $width cm " +
                 "Length: $length cm" +
