bbf81b358bf69e6034d0cbc02fa7b585794dbc0a
Ashal Sharma <ashalsharma68@gmail.com>
2023-09-22 14:21:42 -0500

    GeoQuiz Classwork1

---
+++ apps/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.bignerdranch.android.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
+    private  val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private  var currentIndex =0;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+//        trueButton.setOnClickListener { view: View ->
+//            Toast.makeText(
+//                this,
+//                R.string.correct_toast,
+//                Toast.LENGTH_SHORT)
+//                .show()
+//        }
+
+        trueButton.setOnClickListener { view: View ->
+            Snackbar.make(view, R.string.correct_toast, Snackbar.LENGTH_SHORT).show()
+
+        }
+
+        falseButton.setOnClickListener { view: View ->
+            Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+}
