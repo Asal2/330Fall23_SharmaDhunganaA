16b8c698dc45c9ae46f42ebd26908fb404362d15
Ashal Sharma <ashalsharma68@gmail.com>
2023-10-01 18:22:49 -0500

    Activities and Intents completed

--- apps/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ apps/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.bignerdranch.android.geoquiz
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bignerdranch.android.geoquiz.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
+private const val TAG = "MainActivity"
+private const val CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-
-    private  val questionBank = listOf(
+    private val questionBank = listOf(
         Question(R.string.question_australia, true),
         Question(R.string.question_oceans, true),
         Question(R.string.question_mideast, false),
@@ -20,16 +28,43 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_americas, true),
         Question(R.string.question_asia, true)
     )
+    private var currentIndex = 0
+    private var isCheater = false
 
-    private  var currentIndex =0;
+    //create a launcher so that a result can be returned from the launched activity
+    private val cheatLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){result ->
+        if (result.resultCode == Activity.RESULT_OK){
+            isCheater = result.data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false) ?: false
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        if (savedInstanceState != null) {
+            Log.d(TAG, "savedInstanceState is set.")
+            currentIndex = savedInstanceState.getInt(CURRENT_INDEX_KEY, 0)
+        }
+         //setContentView(R.layout.activity_main)
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val question =
 
+//        trueButton = findViewById(R.id.true_button)
+//        falseButton = findViewById(R.id.false_button)
+
+        //display the initial question
+        binding.questionText.setText(questionBank[currentIndex].testResId)
+
+        //write the next button so that it advances to the next question
+        binding.nextButton.setOnClickListener{
+            currentIndex = (currentIndex + 1) % questionBank.size
+            binding.questionText.setText(questionBank[currentIndex].testResId)
+        }
+
+        // Lambda function SAM
 //        trueButton.setOnClickListener { view: View ->
 //            Toast.makeText(
 //                this,
@@ -38,17 +73,41 @@ class MainActivity : AppCompatActivity() {
 //                .show()
 //        }
 
-        trueButton.setOnClickListener { view: View ->
-            Snackbar.make(view, R.string.correct_toast, Snackbar.LENGTH_SHORT).show()
-
+        binding.trueButton.setOnClickListener { view: View ->
+            checkAnswer(true)
         }
 
-        falseButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT)
-                .show()
+        binding.falseButton.setOnClickListener { view: View ->
+            checkAnswer(false)
         }
+
+        binding.cheatButton.setOnClickListener {
+            // Start the cheat activity
+            val intent = Intent(this, CheatActivity::class.java)
+            val answer = questionBank[currentIndex].answer
+            intent.putExtra(EXTRA_ANSWER_KEY, answer)
+//            startActivity(intent)
+            //use the cheatLauncher instead of startActivity
+            cheatLauncher.launch(intent)
+        }
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        //if they cheated, call them out
+        var resId = when {
+            isCheater -> R.string.judgment_toast
+            userAnswer == correctAnswer -> R.string.correct_toast
+            else -> R.string.incorrect_toast
+        }
+        isCheater = false
+
+        Toast.makeText(this, resId, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState")
+        outState.putInt(CURRENT_INDEX_KEY, this.currentIndex)
     }
 }
--- apps/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/Question.kt
+++ apps/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/Question.kt
@@ -3,3 +3,6 @@ package com.bignerdranch.android.geoquiz
 import androidx.annotation.StringRes
 
 data class Question(@StringRes val testResId: Int, val answer: Boolean)
+//TODO percelize
+//@Parcelize
+// : Parcelable
